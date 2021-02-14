7caaaa1e8c37d773a2e28248004a33b57035f4e2
shota <#shota.gad1@gmail.com>
2021-02-14 21:18:51 +0900

    投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   get 'sessions/new'
+
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   resources :users
   resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
