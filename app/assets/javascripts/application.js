99da57a45eb8c9549ed9485f7ded553916a1cc91
shota <#shota.gad1@gmail.com>
2021-02-14 20:08:15 +0900

    bootstrap導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
